92cecebc0497d13de5d8cc02d8322ffcef665a57
rebelice <yangrebelice@gmail.com>
2023-02-15 16:19:24 +0800

    Update .sdl##LATEST.sql

--- bytebase/prod/.sdl##LATEST.sql
+++ bytebase/prod/.sdl##LATEST.sql
@@ -6,8 +6,8 @@ CREATE TABLE t1(
   d int,
   e int,
   PRIMARY KEY (a, b),
-  CHECK(b > 0),
-  CHECK(c > 0)
+  CHECK check_t1_1 (b > 0),
+  CHECK check_t1_2 (c > 0)
 );
 
 CREATE UNIQUE INDEX uk_t1_a_b_c on t1(a, b, c);
