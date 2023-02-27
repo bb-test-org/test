6c262e3314174a627fee54c2b5f30f588222b7d0
rebelice <yangrebelice@gmail.com>
2023-02-27 18:00:42 +0800

    Update .sdl##LATEST.sql

--- bytebase/prod/.sdl##LATEST.sql
+++ bytebase/prod/.sdl##LATEST.sql
@@ -3,7 +3,7 @@ CREATE TABLE t1(
   a int NOT NULL,
   b int NOT NULL,
   c int                      UNIQUE,
-  d int,
+  d int ,
   x int,
   PRIMARY KEY (a, b),
   CONSTRAINT check_t1_1 CHECK (b > 0),
