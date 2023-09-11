ac536d460ae93534e47776ecd5dd3f12beea3c9e
Bytebase <support@bytebase.com>
2023-09-11 16:25:58 +0800

    [Bytebase] Update latest schema for "db0" after migration 20230911162550
    THIS COMMIT IS AUTO-GENERATED BY BYTEBASE
    
    http://localhost:3000/issue/db0-alter-schema-at09-11-16-25-utc-0800-138?stage=139

--- bytebase/prod/.db0##LATEST.sql
+++ bytebase/prod/.db0##LATEST.sql
@@ -1,14 +1,76 @@
-CREATE TABLE `t_partner_residence_scene` (
-  `id` bigint(20) unsigned NOT NULL  COMMENT 'ID(自增主键)',
-  `scene_id` varchar(191) NOT NULL COMMENT '',
-  `scene_layout_id` varchar(191) NOT NULL COMMENT '',
-  `residence_id` varchar(191) DEFAULT NULL COMMENT '',
-  `content` mediumtext COMMENT '',
-  `remark` mediumtext COMMENT 'remark',
-  `is_banned` tinyint(1) NOT NULL DEFAULT '0' COMMENT '',
-  `created_by` varchar(191) NOT NULL COMMENT '',
-  `created_at` datetime NOT NULL COMMENT '',
-  `updated_by` varchar(191) NOT NULL COMMENT '',
-  `updated_at` datetime NOT NULL COMMENT '',
-  `is_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT ''
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; 
+
+--
+-- Temporary view structure for `curryear_calendar`
+--
+CREATE VIEW `curryear_calendar` AS SELECT
+  1 AS `year`,
+  1 AS `month`,
+  1 AS `start_of_month`,
+  1 AS `end_of_month`;
+
+
+
+
+--
+-- Temporary view structure for `test_json_table`
+--
+CREATE VIEW `test_json_table` AS SELECT
+  1 AS `year`,
+  1 AS `month`,
+  1 AS `start_of_month`,
+  1 AS `end_of_month`;
+
+
+
+
+--
+-- Temporary view structure for `v1`
+--
+CREATE VIEW `v1` AS SELECT
+  1 AS `id`,
+  1 AS `name`;
+
+CREATE TABLE `t` (
+  `id` INT DEFAULT NULL,
+  `name` VARCHAR(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARACTER SET=UTF8MB4 DEFAULT COLLATE=UTF8MB4_0900_AI_CI;
+
+CREATE TABLE `t2` (
+  `id` INT NOT NULL COMMENT 'ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=UTF8MB4 DEFAULT COLLATE=UTF8MB4_0900_AI_CI;
+
+CREATE TABLE `test` (
+  `a` INT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARACTER SET=UTF8MB4 DEFAULT COLLATE=UTF8MB4_0900_AI_CI;
+
+
+
+DROP VIEW IF EXISTS `curryear_calendar`;
+
+
+--
+-- View structure for `curryear_calendar`
+--
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `curryear_calendar` AS select `y_data`.`year` AS `year`,`m_data`.`month` AS `month`,date_format(concat(`y_data`.`year`,'-',lpad(`m_data`.`month`,2,'0'),'-','01'),'%Y-%m-%d') AS `start_of_month`,last_day(concat(`y_data`.`year`,lpad(`m_data`.`month`,2,'0'),'01')) AS `end_of_month` from (json_table('[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]', '$[*]' columns (`month` int path '$')) `m_data` join (select year(curdate()) AS `year`) `y_data`);
+
+
+
+DROP VIEW IF EXISTS `test_json_table`;
+
+
+--
+-- View structure for `test_json_table`
+--
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `test_json_table` AS select `y_data`.`year` AS `year`,`m_data`.`month` AS `month`,date_format(concat(`y_data`.`year`,'-',lpad(`m_data`.`month`,2,'0'),'-','01'),'%Y-%m-%d') AS `start_of_month`,last_day(concat(`y_data`.`year`,lpad(`m_data`.`month`,2,'0'),'01')) AS `end_of_month` from (json_table('[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]', '$[*]' columns (`month` int path '$')) `m_data` join (select year(curdate()) AS `year`) `y_data`);
+
+
+
+DROP VIEW IF EXISTS `v1`;
+
+
+--
+-- View structure for `v1`
+--
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v1` AS select `t`.`id` AS `id`,`t`.`name` AS `name` from `t`;
+
